0341c90cd32ec9f9dbe33df2da1912f7ca6704ea
Andy Prickett <andy@andyprickett.com>
2018-09-23 18:13:03 -0700

    Lesson 2

--- quiz.js
+++ quiz.js
@@ -24,30 +24,68 @@ let questions = [
 ];
 
 $(document).ready(function() {
-  
-  $('.start a').click(function(e) {
+  $(".start a").click(function(e) {
     e.preventDefault();
-    $('.start').hide();
-    $('.quiz').show();
+    $(".start").hide();
+    $(".quiz").show();
     showQuestion();
   });
-  
-  $('.quiz ul').on('click','li',function() {
-    $('.selected').removeClass('selected');
-    $(this).addClass('selected');
+
+  $(".quiz ul").on("click", "li", function() {
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
   });
-  
+
+  $(".quiz a").click(function(e) {
+    e.preventDefault();
+    if ($("li.selected").length) {
+      let guess = parseInt($("li.selected").attr("id"));
+      checkAnswer(guess);
+    } else {
+      alert("Please select an answer");
+    }
+  });
+
+  $(".summary a").click(function(e) {
+    e.preventDefault();
+    restartQuiz();
+  });
 });
 
 function showQuestion() {
   let question = questions[currentQuestion];
-  $('.quiz h2').text(question.title);
-  $('.quiz ul').html('');
+  $(".quiz h2").text(question.title);
+  $(".quiz ul").html("");
   for (var i = 0; i < question.answers.length; i++) {
-    $('.quiz ul').append(`<li id="${i}">${question.answers[i]}</li>`)
+    $(".quiz ul").append(`<li id="${i}">${question.answers[i]}</li>`);
   }
 }
 
-function checkAnswer() {}
+function checkAnswer(guess) {
+  let question = questions[currentQuestion];
+  if (question.correct === guess) {
+    score++;
+  }
+  currentQuestion++;
+  if (currentQuestion >= questions.length) {
+    showSummary();
+  } else {
+    showQuestion();
+  }
+}
 
-function showSummary() {}
+function showSummary() {
+  $(".quiz").hide();
+  $(".summary").show();
+  $(".summary p").text(
+    `Congrats you scored ${score} out of ${questions.length} correct!`
+  );
+}
+
+function restartQuiz() {
+  $(".summary").hide();
+  $(".quiz").show();
+  score = 0;
+  currentQuestion = 0;
+  showQuestion();
+}
